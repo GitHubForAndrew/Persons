ea9c6aa446518b818254bd9e1ec7951e268c20e3
Андрей Ярославцев <andrew.yaroslavcev@live.ru>
2016-11-29 10:47:51 +0500

    Done

--- Persons/Concrete/Repository.cs
+++ Persons/Concrete/Repository.cs
@@ -8,7 +8,7 @@ using Persons.Models;
 
 namespace Persons.Concrete
 {
-    public class PersonsRepository<T> : IRepository<T> where T:Person
+    public class PersonsRepository
     {
         private List<Person> list;
 
@@ -26,22 +26,40 @@ namespace Persons.Concrete
                 new Person { ID = 9, Age = 37, Name = "Erik" },
                 new Person { ID = 10, Age = 26, Name = "Liza" },
                 new Person { ID = 11, Age = 24, Name = "Gomer" },
-                new Person { ID = 12, Age = 34, Name = "Petr" },
+                new Person { ID = 12, Age = 34, Name = "Petr" }
             };
         }
 
-
-        public Task<T> GetItemAsync(int Id)
+        public Task<List<Person>> GetMorePersosns()
         {
-            return Task.Run<T>(() =>
+            return Task.Run(() =>
             {
-                return list.FirstOrDefault(i => i.ID == Id) as T;
+                return new List<Person> {
+                new Person { ID = 13, Age = 34, Name = "Sergey" },
+                new Person { ID = 14, Age = 26, Name = "Luiza" },
+                new Person { ID = 15, Age = 24, Name = "Marta" },
+                new Person { ID = 16, Age = 48, Name = "Pascal" },
+                new Person { ID = 17, Age = 41, Name = "Jordan" },
+                new Person { ID = 18, Age = 38, Name = "York" },
+                new Person { ID = 19, Age = 25, Name = "Nali" },
+                new Person { ID = 20, Age = 29, Name = "Jim" },
+                new Person { ID = 21, Age = 36, Name = "Vanessa" },
+                new Person { ID = 22, Age = 22, Name = "Vikky" }
+                };
+            });   
+        }
+
+        public Task<Person> GetItemAsync(int Id)
+        {
+            return Task.Run(() =>
+            {
+                return list.FirstOrDefault(i => i.ID == Id);
             });
         }
 
-        public Task<List<T>> GetListAsync()
+        public Task<List<Person>> GetListAsync()
         {
-            return Task.Run(() => list as List<T>);
+            return Task.Run(() => list);
         }
     }
 }
--- Persons/Controllers/PersonsController.cs
+++ Persons/Controllers/PersonsController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Persons.Abstract;
+using Persons.Concrete;
+using Persons.Models;
+using System.Threading.Tasks;
 
 namespace Persons.Controllers
 {
     public class PersonsController : Controller
     {
-        // GET: Persons
-        public ActionResult Index()
+        private PersonsRepository repo;
+
+        public PersonsController()
         {
-            return View();
+            repo = new PersonsRepository();
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Title = "Persons";
+            var items = await repo.GetListAsync();
+            return View(items);
+        }
+        
+        //[AcceptVerbs(HttpVerbs.Get)]
+        public async Task<PartialViewResult> GetMorePersons()
+        {
+            var list = await repo.GetMorePersosns();
+            return PartialView("PersonsPartial", list);
         }
     }
 }
